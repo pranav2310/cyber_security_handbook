171361a3b2a482b6364170efb3745cfa486107ac
Pranav Kumar Varshney <vpranav2310@gmail.com>
2025-06-17 23:16:30 +0530

    Naming the App

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cybersecurity Guide',
+      title: 'CyberGuard Handbook',
       theme: ThemeData(useMaterial3: true),
       home: StartingGrid(),
     );
